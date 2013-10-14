154f60e974b694a052f3f9596fa6d78726a92bc4
Matias Bjarland <matias@iteego.com>
2013-10-14 14:21:41 +0200

    adding some code for executing an external groovy script

--- src/main/groovy/myscript.groovy
+++ src/main/groovy/myscript.groovy
@@ -5,6 +5,6 @@
  */
 Integer sleepTime = (args[0] as Integer ?: 500)
 
-println "Sleeping ${sleepTime}ms"
+println "$threadName sleeping for ${sleepTime}ms"
 sleep(sleepTime)
-println "I have awoken!"
+println "I have awoken with thread $threadName!"
